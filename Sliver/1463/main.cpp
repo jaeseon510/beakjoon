9ed4db4e5f0ec81d166796e9e573356069e3a443
Yang jaeseon <111330037+jaeseon510@users.noreply.github.com>
2024-02-17 22:26:27 +0900

    Update main.cpp

--- Sliver/1463/main.cpp
+++ Sliver/1463/main.cpp
@@ -1,1 +1,34 @@
-
+#include<iostream>
+using namespace std;
+int d[1000001];
+int dp(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    if (d[n] > 0) {
+        return d[n];
+    }
+    d[n] = dp(n - 1) + 1;
+    if (n % 2 == 0) {
+        int temp = dp(n / 2) + 1;
+        if (d[n] > temp) {
+            d[n] = temp;
+        }
+    }
+    if (n % 3 == 0) {
+        int temp = dp(n / 3) + 1;
+        if (d[n] > temp) {
+            d[n] = temp;
+        }
+    }
+    return d[n];
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << dp(n) << '\n';
+    return 0;
+}
